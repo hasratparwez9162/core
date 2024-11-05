a2bc69ebae5282b0a3209ac1e82b745e9f79d9f5
Hasrat Parwez <91671497+hasratparwez9162@users.noreply.github.com>
2024-11-05 15:44:05 +0530

    Add More card status

--- src/main/java/com/bank/core/entity/CardStatus.java
+++ src/main/java/com/bank/core/entity/CardStatus.java
@@ -6,5 +6,7 @@ public enum CardStatus {
     EXPIRED,         // Card has passed its expiry date
     PENDING_ACTIVATION,  // New card waiting to be activated
     SUSPENDED,       // Temporarily suspended
-    CANCELLED        // Permanently cancelled
-}
+    CANCELLED,        // Permanently cancelled
+    PENDING_BLOCK,          // Request to block the card is pending
+    PENDING_UNBLOCK        // Request to unblock the card is pending
+    }
